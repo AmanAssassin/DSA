7a948dddc50ee1af56761ae7f8a7733e6acb8bdd
Aman Maurya <amanmaurya112233@gmail.com>
2021-10-30 14:32:55 +0530

    added

--- OOP/Student.cpp
+++ OOP/Student.cpp
@@ -1,8 +1,54 @@
-class Student
-{
+class Student {
 
-public:
-    int rollNumber;
-    int age;
+	public :
+
+	int rollNumber;
+	
+	private :
+
+	int age;
+
+
+	public :
+
+
+	// Default constructor
+	/*Student() {
+		cout << "Constreuctor called ! "<< endl;
+	}*/
+
+	// Parameterized constructor
+	Student(int rollNumber) {
+		cout << "Constructor 2 called !" << endl;
+	
+		this -> rollNumber = rollNumber;
+	}
+
+	Student(int a, int r) {
+		cout << "this : " << this << endl;
+		cout << "Constructor 3 called ! " << endl;
+		this -> age = a;
+		this -> rollNumber = r;
+	}
+
+	void display() {
+		cout << age << " " << rollNumber << endl;
+	}
+
+	int getAge() {
+		return age;
+	}
+
+	void setAge(int a, int password) {
+		if(password != 123) {
+			return;
+		}
+		if(a < 0) {
+			return;
+		}
+		age = a;
+	}
 
 };
+
+
--- OOP/studentUse.cpp
+++ OOP/studentUse.cpp
@@ -2,27 +2,38 @@
 using namespace std;
 #include "Student.cpp"
 
-int main()
-{
-    // Create objects statically
-    Student s1;
-    Student s2;
 
-    Student s3, s4, s5;
+int main() {
 
-    s1.age = 24;
-    s1.rollNumber = 101;
+	Student s1(10, 1001);
+	cout << "Address of s1 : " << &s1 << endl;
 
-    cout << s1.age << endl;
-    cout << s1.rollNumber << endl;
+	Student s2(20);
+	s2.display();
 
-    s2.age = 30;
 
-    // Create objects dynamicaaly
-    Student *s6 = new Student;
-    (*s6).age = 23;
-    (*s6).rollNumber = 104;
+	/*
+	Student s1;
 
-    s6->age = 23;
-    s6->rollNumber = 104;
+	s1.display();
+
+	Student s2;
+
+	Student *s3 = new Student;
+	s3 -> display();
+
+
+	cout << "Parametrized constructors Demo" << endl;
+	Student s4(10);
+
+	s4.display();
+
+	Student *s5 = new Student(101);
+	s5 -> display();
+
+	Student s6(20, 1002);
+	s6.display()
+	*/
+	
 }
+
