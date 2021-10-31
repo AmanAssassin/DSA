bb6db70310a5ea544f34756911652d8d05ee8734
Aman Maurya <amanmaurya112233@gmail.com>
2021-10-31 17:58:58 +0530

    opps updated

---
+++ OOP/CheckAll.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include "Student.cpp"
+
+
+int main() {
+
+    Student s1;
+    Student s2(101);
+    Student s2(101,1001);
+    Student s4(s3);
+    s1 = s2;
+    Student s5=s4;
+}
--- OOP/studentUse.cpp
+++ OOP/studentUse.cpp
@@ -12,6 +12,21 @@ int main() {
 	s2.display();
 
 
+	Student s1(10, 1001);
+	Student s2(20, 2001);
+
+	Student *s3 = new Student(30, 3001);
+
+	s2 = s1;
+
+	*s3 = s1;
+
+	s2 = *s3;
+
+	delete s3;
+
+
+
 	/*
 	Student s1;
 
