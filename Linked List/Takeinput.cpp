3e9eba12e9fc41274a63d1a5d920f0c92ec01689
Aman Maurya <amanmaurya112233@gmail.com>
2021-11-26 11:47:44 +0530

    Took input from ll and print

--- Linked List/Takeinput.cpp
+++ Linked List/Takeinput.cpp
@@ -2,30 +2,45 @@
 #include "Node.cpp"
 using namespace std;
 
-void print(Node *Head)
-{
-    Node *temp = Head;
-    while (temp != NULL)
-    {
-        cout << temp->data << " ";
-        temp = temp->next;
-    }
-    temp = Head;
-    while (temp != NULL)
-    {
+Node* takeInput() {
+	int data;
+	cin >> data;
+	Node *Head = NULL;
+	while(data != -1) {
+		Node *newNode = new Node(data);
+		if(Head == NULL) {
+			Head = newNode;
+		}
+		else {
+			Node *temp = Head;
+			while(temp -> next != NULL) {
+				temp = temp -> next;
+			}
+			temp -> next = newNode;
+		}
 
-        cout << temp->data << " ";
-        Head = temp->next;
-    }
+		cin >> data;
+	}
+	return Head;
+}
+
+void print(Node *head) {
+	while(head != NULL) {
+		cout << head -> data << " ";
+		head = head -> next;
+	}
 }
 
 int main()
 {
-    // statically create Node
 
+    Node *Head = takeInput();
+    print(Head);
+    /*
+    // statically create Node
     Node n1(1); //this create a node object with 1 as a value with Null as a adress
 
-    Node *Head = &n1; // storing the address of the head node i.e. first n1  ||its just a pointer variable not a new node
+    Node *Head = &n1; // storing the address of the Head node i.e. first n1  ||its just a pointer variable not a new node
 
     //similarly this create a node object with 1 as a value with Null as a adress
     // aftre the above lines there are two nodes with 1 and 2 value both having self some address but storing Null adress inside the object of Node so now we want them to connect them
@@ -34,7 +49,7 @@ int main()
     Node n4(4);
     Node n5(5);
 
-    // copying fronts address in previous node and head having address of first and last is having NULL
+    // copying fronts address in previous node and Head having address of first and last is having NULL
     n1.next = &n2;
     n2.next = &n3;
     n3.next = &n4;
